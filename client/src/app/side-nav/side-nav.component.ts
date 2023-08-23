fe2267b951eec2281cabbc3c97e1e78eed082edf
samukelolukhele <samukelo.business@gmail.com>
2023-08-23 14:33:17 +0200

    FEAT: Side-nav initialized.

--- client/src/app/side-nav/side-nav.component.ts
+++ client/src/app/side-nav/side-nav.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  faDashboard,
+  faCalendar,
+  faPen,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.css']
+  styleUrls: ['./side-nav.component.sass'],
 })
 export class SideNavComponent implements OnInit {
+  faDashboard = faDashboard;
+  faCalendar = faCalendar;
+  faPen = faPen;
+  faUser = faUser;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
